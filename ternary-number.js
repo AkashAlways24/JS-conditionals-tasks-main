21e19098f04969e6fbdde1c7fbb8fc85866167f2
AkashOffic <md.arhamhossainakash55@gmail.com>
2025-03-24 10:08:58 +0600

    ternary-number.js task done

--- ternary-number.js
+++ ternary-number.js
@@ -10,3 +10,11 @@ write a simple if-else.
 also, write it using ternary operator.
 
  */
+
+let num1 = 10;
+let num2 = 21;
+
+let result = num1 < num2 ? num1 * num2 : num1 + num2;
+
+console.log(result);
+
